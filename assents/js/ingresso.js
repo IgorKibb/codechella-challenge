3f64929cf822cd2039e52356d7192c805f8ded73
IgorKibb <igorkibb@hotmail.com>
2023-03-19 09:56:12 -0300

    implementando a validação de idade

--- assents/js/ingresso.js
+++ assents/js/ingresso.js
@@ -5,17 +5,17 @@ console.log(inputNascimento)
 
 
 export default formulario.addEventListener('submit', (e) => {
-   e.preventDefault();
+   //e.preventDefault();
    //return false}
    const nomeImput = nome.value;
    localStorage.setItem("nomeImput", JSON.stringify(nomeImput));
    const nomeIngresso = JSON.parse(localStorage.getItem("nomeImput"));
 
    //enviando a data para localstorage. Vamos tentar fazer diferente do chat gpt
-   const dataDonascimento = inputNascimento.value;
-   localStorage.setItem("dataNascimento", JSON.stringify(dataDonascimento));
+   //const dataDonascimento = inputNascimento.value;
+   //localStorage.setItem("dataNascimento", JSON.stringify(dataDonascimento));
 })
-/*chat GPT/BING
+//chat GPT/BING
 // Get the input element
 var input = document.getElementById("nascimento");
 
@@ -37,12 +37,10 @@ input.addEventListener("change", function() {
   }
 
   // Check if the difference is at least 16
-  if (diff >= 16) {
-    alert("You are over 16 years old.");
-  } else {
-    alert("You are under 16 years old.");
+  if (diff <= 16) {
+    alert("Você deve ter mais de 16 anos!");
   }
-});*/
+});
 
 
 
@@ -54,7 +52,7 @@ input.addEventListener("change", function() {
    }
 }*/
 
-function validaDtaNascimento(){
+/*function validaDtaNascimento(){
    const dataNascimento = new Date (inputNascimento.value);
    if(dataNascimento >= maiorQue16){
       return console.log('pode participar do festival')
@@ -66,4 +64,28 @@ function maiorQue16(data){
    const dataAtual = new Date(); //para colocar a data do dia, não precisa colocar o valor, assim ele colocará automaticamente.
    const dataMais16 = new Date(data.getUTCFullYear() + 16, data.getUTCMonth(), data.getUTCDate());//getUTCDate pega o dia.
    return dataMais16 <= dataAtual;
-};
+};*/
+
+//moment.js lib
+/*function validateDate(date) {
+   var inputDate = moment(date); // create a moment object from the input value
+   var currentDate = moment(); // get the current date as a moment object
+   var age = currentDate.diff(inputDate, "years"); // calculate the difference in years
+   if (age >= 16) {
+     return true; // valid date
+   } else {
+     return false; // invalid date
+   }
+ }
+
+
+ var dobInput = document.querySelector("input[name='dob']"); // get the input element by name
+
+dobInput.addEventListener("change", function() { // add an event listener for change event
+  var dobValue = dobInput.value; // get the input value
+  var isValid = validateDate(dobValue); // call the validateDate function with the input value
+  if (!isValid) {
+    alert("Você deve ter mais de 16 anos para participar!"); // show a message if valid
+    // enable other elements here if needed
+  }
+});*/
